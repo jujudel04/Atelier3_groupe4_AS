d998863e1f2ce97db010e661b6e5dee89333dce2
jujudel04 <78476197+jujudel04@users.noreply.github.com>
2021-05-21 11:52:27 +0200

    Add files via upload

--- market.zip_expanded/market/src/main/java/groupe4/market/controller/MarketController.java
+++ market.zip_expanded/market/src/main/java/groupe4/market/controller/MarketController.java
@@ -1,6 +1,7 @@
 package groupe4.market.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,18 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import groupe4.market.service.MarketService;
+
 @Controller
 public class MarketController {
 
-	
-	
 	@RequestMapping(method = RequestMethod.GET, value = "/card/showall/{authenticateid}")
 	public String showAllCards(Model model, @PathVariable Integer authenticateid) {
-		if (UserService.isAuthenticate(authenticateid)) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> response = restTemplate.getForEntity("http://localhost:8080/User" + "/1", UserDto.class);
+		if (response.isAuthenticate(authenticateid)) {
 			model.addAttribute("authenticateid", authenticateid);
-			User user = userService.getUser(authenticateid);
-			model.addAttribute("name", user.getName());
-			model.addAttribute("balance", user.getAccountBalance());
+			model.addAttribute("name", response.getName());
+			model.addAttribute("balance", response.getAccountBalance());
 			return "cardlist";
 		} else {
 			model.addAttribute("nok", "unreconnized user");
@@ -31,11 +32,13 @@ public class MarketController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/card/buy/{id}/{authenticateid}")
-	public String buyCard(Model model, @PathVariable Integer id, @PathVariable Integer authenticateid) {
-		if (UserService.isAuthenticate(authenticateid)) {
-			model.addAttribute("authenticateid", authenticateid);
-			Card card = cardService.getCard(id);
-			userService.addCard(card, authenticateid);
+	public String buyCard(Model model,@PathVariable Integer id, @PathVariable Integer authenticateid) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> response = restTemplate.getForEntity("http://localhost:8080/User" + "/1", UserDto.class);
+		if (response.isAuthenticate(authenticateid)) {
+			response.addAttribute("authenticateid", authenticateid);
+			ResponseEntity<CardDto> card = restTemplate.getForEntity("http://localhost:8080Card" + "/1", CardDto.class);
+			response.addCard(card, authenticateid);
 			return "redirect:/user/show/" + authenticateid;
 		} else {
 			model.addAttribute("nok", "unreconnized user");
@@ -45,11 +48,12 @@ public class MarketController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/card/sellshowall/{authenticateid}")
 	public String sellshowAllCards(Model model, @PathVariable Integer authenticateid) {
-		if (UserService.isAuthenticate(authenticateid)) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> response = restTemplate.getForEntity("http://localhost:8080/User" + "/1", UserDto.class);
+		if (response.isAuthenticate(authenticateid)) {
 			model.addAttribute("authenticateid", authenticateid);
-			User user = userService.getUser(authenticateid);
-			model.addAttribute("name", user.getName());
-			model.addAttribute("balance", user.getAccountBalance());
+			model.addAttribute("name", response.getName());
+			model.addAttribute("balance", response.getAccountBalance());
 			return "sellcardlist";
 		} else {
 			model.addAttribute("nok", "unreconnized user");
@@ -58,11 +62,14 @@ public class MarketController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/card/sell/{id}/{authenticateid}")
-	public String sellCard(Model model, @PathVariable Integer id, @PathVariable Integer authenticateid) {
-		if (UserService.isAuthenticate(authenticateid)) {
+	public String sellCard(Model model,@PathVariable Integer id, @PathVariable Integer authenticateid) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> response = restTemplate.getForEntity("http://localhost:8080/User" + "/1", UserDto.class);
+		if (response.isAuthenticate(authenticateid)) {
 			model.addAttribute("authenticateid", authenticateid);
-			Card card = cardService.getCard(id);
-			userService.removeCard(card, authenticateid);
+			response.addAttribute("authenticateid", authenticateid);
+			ResponseEntity<CardDto> card = restTemplate.getForEntity("http://localhost:8080Card" + "/1", CardDto.class);
+			UserDto.removeCard(card, authenticateid);
 			return "redirect:/user/show/" + authenticateid;
 		} else {
 			model.addAttribute("nok", "unreconnized user");
@@ -70,18 +77,63 @@ public class MarketController {
 		}
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/card/play/{authenticateid}")
-	public String listUserCards(Model model, @PathVariable Integer authenticateid) {
-		if (UserService.isAuthenticate(authenticateid)) {
-			model.addAttribute("authenticateid", authenticateid);
-			User user = userService.getUser(authenticateid);
-			model.addAttribute("name", user.getName());
-			model.addAttribute("balance", user.getAccountBalance());
+	public static class UserDto {
+		private Integer userID;
+		private String card;
+		private Integer accountBalance;
 
-			return "room";
-		} else {
-			model.addAttribute("nok", "unreconnized user");
-			return "index2";
+		public UserDto() {
+		}
+
+		public Integer getUserID() {
+			return userID;
+		}
+
+		public void setUserID(Integer userID) {
+			this.userID = userID;
+		}
+
+		public String getCards() {
+			return card;
+		}
+
+		public void setCards(String cards) {
+			this.card = cards;
+		}
+
+		public Integer getAccountBalance() {
+			return accountBalance;
+		}
+
+		public void setAccountBalance(Integer accountBalance) {
+			this.accountBalance = accountBalance;
 		}
 	}
+
+	public static class CardDto {
+		private Integer id;
+		private Integer price;
+
+		public CardDto() {
+
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public Integer getPrice() {
+			return price;
+		}
+
+		public void setPrice(Integer price) {
+			this.price = price;
+		}
+
+	}
+
 }
--- market.zip_expanded/market/src/main/java/groupe4/market/service/MarketService.java
+++ market.zip_expanded/market/src/main/java/groupe4/market/service/MarketService.java
@@ -2,17 +2,20 @@ package groupe4.market.service;
 
 import java.util.List;
 
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class MarketService {
 
-	
-	
-	
-
-	public void addCard(Card card, Integer userId) {
-		User user = this.getUser(userId);
+	public void addCard(Integer userId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> user = restTemplate.getForEntity("http://localhost:8080/User", UserDto.class);
+		ResponseEntity<CardDto> card = restTemplate.getForEntity("http://localhost:8081/Card", CardDto.class);
+		user = user.getUser(userId);
 		if (user != null) {
 			user.addCard(card.getId());
 			user.updateBalance(-card.getPrice());
@@ -21,13 +24,76 @@ public class MarketService {
 			this.saveUser(user);
 		}
 	}
-	
-	public void removeCard(Card card, Integer userId) {
-		User user = this.getUser(userId);
+
+	public void removeCard(Integer userId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserDto> user = restTemplate.getForEntity("http://localhost:8080/User", UserDto.class);
+		ResponseEntity<CardDto> card = restTemplate.getForEntity("http://localhost:8081/Card", CardDto.class);
+		user = user.getUser(userId);
 		if (user != null) {
 			user.removeCard(card.getId());
 			user.updateBalance(+card.getPrice());
 			this.saveUser(user);
 		}
 	}
-}
+
+	public static class UserDto {
+		private Integer userID;
+		private String card;
+		private Integer accountBalance;
+
+		public UserDto() {
+		}
+
+		public Integer getUserID() {
+			return userID;
+		}
+
+		public void setUserID(Integer userID) {
+			this.userID = userID;
+		}
+
+		public String getCards() {
+			return card;
+		}
+
+		public void setCards(String cards) {
+			this.card = cards;
+		}
+
+		public Integer getAccountBalance() {
+			return accountBalance;
+		}
+
+		public void setAccountBalance(Integer accountBalance) {
+			this.accountBalance = accountBalance;
+		}
+	}
+		public static class CardDto {
+			private Integer id;
+			private Integer price;
+
+			public CardDto() {
+
+			}
+
+			public Integer getId() {
+				return id;
+			}
+
+			public void setId(Integer id) {
+				this.id = id;
+			}
+
+			public Integer getPrice() {
+				return price;
+			}
+
+			public void setPrice(Integer price) {
+				this.price = price;
+			}
+
+		}
+
+	}
+
