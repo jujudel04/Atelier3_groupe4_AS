f34d62bd6aebf1c3495898c5de1c10dbb8fdf1e4
jujudel04 <78476197+jujudel04@users.noreply.github.com>
2021-05-21 11:53:27 +0200

    Add files via upload

--- pageweb.zip_expanded/pageweb/src/main/java/groupe4/pageweb/controller/PageWebController.java
+++ pageweb.zip_expanded/pageweb/src/main/java/groupe4/pageweb/controller/PageWebController.java
@@ -1,5 +1,13 @@
 package groupe4.pageweb.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PageWebController {
 
+	
+	public void affichePages(String nom,Integer authenticateid){
+		return "redirect:"+nom + authenticateid;
+		
+	}
 }
